0b9c3c74aa2c45b10c8fcf370dbd406c5364acdb
cinnamorollsanrio <princesspadaloy45@gmail.com>
2025-03-21 20:30:19 +0800

    Updated files with new changes

---
+++ CS1A_MTExam_Tallayo/userProfile.js
@@ -0,0 +1,11 @@
+let fullName = prompt("Enter your full name:"); // let for variables
+let age = prompt("Enter your age:"); 
+let favoriteNumber = prompt("Enter your favorite number:"); // use prompt to ask the user
+let favoriteColor = prompt("Enter your favorite color:");
+
+// Output using console.log
+console.log("User Profile:"); // console.log to print
+console.log("Full Name: " + fullName); // use + to add or join(concatenating) text and variables into one string
+console.log("Age: " + age);
+console.log("Favorite Number: " + favoriteNumber);
+console.log("Favorite Color: " + favoriteColor);
