0d3f9b953bbb6e8a053caba921aaab8a05c909f9
cinnamorollsanrio <princesspadaloy45@gmail.com>
2025-03-21 21:56:15 +0800

    Create 3_userAge.js

---
+++ CS1A_MTExam_Tallayo/3_userAge.js
@@ -0,0 +1,16 @@
+let age = prompt("Enter your age:"); // ask user
+age = Number(age); // Convert input to a number
+
+if (age < 5) {
+    console.log("Toddler (under 5 years)");
+} else if (age >= 5 && age <= 12) {
+    console.log("Child (5-12 years)");
+} else if (age >= 13 && age <= 19) {
+    console.log("Teenager (13-19 years)");
+} else if (age >= 20 && age <= 35) {
+    console.log("Young Adult (20-35 years)");
+} else if (age >= 36 && age <= 60) {
+    console.log("Middle-Aged (36-60 years)");
+} else {
+    console.log("Senior (over 60 years)"); // to print
+}
